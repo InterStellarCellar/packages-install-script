ee421d8d97c1219fd639236bb92fac1f70b65891
InterStellarCellar <123860220+InterStellarCellar@users.noreply.github.com>
2024-11-14 21:40:44 +1100

    Update install.sh code and fixed the file name

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo pacman -Sy
+sudo pacman -S code
+sudo pacman -S discord
+sudo pacman -S code
+sudo pacman -S --needed base-devel git
+sudo pacman -S yay
+sudo pacman -S steam
+sudo pacman -S cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python python3
+sudo pacman -S alacritty
+yay -S google-chrome
+yay -Syu
--- installshit.sh
+++
@@ -1,7 +0,0 @@
-sudo pacman -Sy
-sudo pacman -S code
-sudo pacman -S discord
-sudo pacman -S code
-sudo pacman -S --needed base-devel git
-yay -S google-chrome
-yay -Syu
